dba3a4dc7fb37320d827d71cb44b9a507963ecf5
Daniel Carneiro <daniel.carneiro@cads.co.uk>
2021-06-26 22:41:41 +0100

    chore: created conteudo

---
+++ miniCMS/Models/Conteudo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace miniCMS.Models
+{
+    public class Conteudo
+    {
+        public int Id { get; set; }
+        [StringLength(50)]
+        public string Pagina { get; set; }
+        [StringLength(50)]
+        public string Titulo { get; set; }
+        public string Texto { get; set; }
+        [StringLength(50)]
+        public string Autor { get; set; }
+        public DateTime Data { get; set; }
+    }
+}
