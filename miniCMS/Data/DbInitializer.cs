4236f09b257d164afec8af8f36358c64b5a51d27
Daniel Carneiro <daniel.carneiro@cads.co.uk>
2021-06-26 22:49:47 +0100

    chore: added database seeds and image

--- miniCMS/Data/DbInitializer.cs
+++ miniCMS/Data/DbInitializer.cs
@@ -1,4 +1,7 @@
-﻿
+﻿using miniCMS.Models;
+using System;
+using System.Linq;
+
 namespace miniCMS.Data
 {
     public class DbInitializer
@@ -6,6 +9,24 @@ namespace miniCMS.Data
         public static void Initialize(DbMiniCMSContext context)
         {
             context.Database.EnsureCreated();
+            // Look for any conteudo.
+            if (context.Conteudos.Any())
+            {
+                return; // DB has been seeded
+            }
+            Conteudo novoConteudo = new Conteudo
+            {
+                Pagina = "Home",
+                Titulo = "<h1>Home</h1>",
+                Texto = "<p>Bem vindo ao <b>Sistema miniCMS</b></p>" +
+                        "<p>Todo o conteudo do site está numa <b>base de dados</b></p>" +
+                        "<img src = '/cms/media/minicms.jpg' /> ",
+                Autor = "miniCMS",
+                Data = DateTime.Now
+            };
+            context.Conteudos.Add(novoConteudo);
+            context.SaveChanges();
+
         }
     }
 }
