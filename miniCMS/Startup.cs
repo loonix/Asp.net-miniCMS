6f9a65baf7100cfb170cb72596902da048fcfe93
Daniel Carneiro <daniel.carneiro@cads.co.uk>
2021-06-26 22:42:14 +0100

    chore: added initialization of db on startup

--- miniCMS/Startup.cs
+++ miniCMS/Startup.cs
@@ -1,8 +1,10 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using miniCMS.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,6 +24,7 @@ namespace miniCMS
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<DbMiniCMSContext>(options => options.UseSqlServer(Configuration.GetConnectionString("miniCMSConnection")));
             services.AddControllersWithViews();
         }
 
