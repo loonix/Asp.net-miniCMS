55ddba35cf058773dd28e780390963da14b837a1
Daniel Carneiro <daniel.carneiro@cads.co.uk>
2021-06-26 22:41:32 +0100

    chore: added db context class

---
+++ miniCMS/Data/DbMiniCMSContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using miniCMS.Models;
+
+namespace miniCMS.Data
+{
+    public class DbMiniCMSContext : DbContext
+    {
+        public DbMiniCMSContext(DbContextOptions<DbMiniCMSContext> options) : base(options)
+        {
+        }
+        public DbSet<Conteudo> Conteudos { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Conteudo>().ToTable("Conteudo");
+        }
+    }
+}
